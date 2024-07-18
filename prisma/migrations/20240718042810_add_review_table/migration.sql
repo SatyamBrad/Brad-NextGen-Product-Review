a2de0dd6be41490fd5d5a44d54adb4f4fd2a5fe2
Shaunak Chandra <79399986+Kingsky1t@users.noreply.github.com>
2024-07-18 14:54:34 +0530

    restart point

---
+++ prisma/migrations/20240718042810_add_review_table/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "shop" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "scope" TEXT,
+    "expires" DATETIME,
+    "accessToken" TEXT NOT NULL,
+    "userId" BIGINT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+    "locale" TEXT,
+    "collaborator" BOOLEAN DEFAULT false,
+    "emailVerified" BOOLEAN DEFAULT false
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "shop" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "customerId" TEXT DEFAULT 'Guest',
+    "customerName" TEXT DEFAULT 'Anonymous',
+    "reviewTitle" TEXT DEFAULT '',
+    "reviewDescription" TEXT DEFAULT '',
+    "starRating" INTEGER DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT DEFAULT 'Published'
+);
+
+-- CreateTable
+CREATE TABLE "ReviewImage" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "imageUrl" TEXT,
+    "reviewId" INTEGER NOT NULL,
+    CONSTRAINT "ReviewImage_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "Review" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
